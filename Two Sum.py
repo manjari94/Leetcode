ef9a4801a3c5bd59479bd3f9e6d9ab5b41705e84
Manjari Misra <manjarimisra94@gmail.com>
2022-06-15 19:52:49 -0400

    Update Two Sum.py

--- Two Sum.py
+++ Two Sum.py
@@ -1,6 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         #brute force unoptimized
+        # Time complexity : O(n^2)
         output = []
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
