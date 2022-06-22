d880e408ebc92f32c2d467501d9d09438edecb2a
Manjari Misra <manjarimisra94@gmail.com>
2022-06-22 03:46:37 -0400

    Update Two Sum.py

--- Two Sum.py
+++ Two Sum.py
@@ -1,12 +1,32 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         #brute force unoptimized
-        # Time complexity : O(n^2)
-        output = []
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    output = [i,j]
-                    return output
-        return -1
+#         output = []
+#         for i in range(len(nums)):
+#             for j in range(i+1,len(nums)):
+#                 if nums[i]+nums[j]==target:
+#                     output = [i,j]
+#                     return output
+#         return -1
+        #hashmap optimized approach
+        # hashmap = {}
+        # result = []
+        # for i in range(len(nums)):
+        #     idx = target-nums[i]
+        #     if idx in hashmap:
+        #         result = [i,hashmap[idx]]
+        #     else:
+        #         hashmap[nums[i]]=i
+        # return result
+        #two pointer approach
+        nums.sort()
+        pointer1 = 0
+        pointer2 = len(nums)-1
+        while pointer1<pointer2:
+            if nums[pointer1]+nums[pointer2]==target:
+                return [pointer1,pointer2]
+            elif nums[pointer1]+nums[pointer2]<target:
+                pointer1 += 1
+            elif nums[pointer1]+nums[pointer2]>target:
+                pointer2 -= 1
             
